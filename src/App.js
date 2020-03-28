72bd8907bfd37cec6a0a99b5fcd2c08c0786d0a8
Pasha <prostobux@yandex.ru>
2020-03-28 22:53:58 +0300

    project crated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CountryCard extends React.Component {
+  render(){
+    var items = []
+    const { getCode } = require('country-list');
+    for (let i = 0; i < this.props.api.slice(-3).length; i++) {
+      const countryCode = getCode(this.props.api[i].country);
+      items.push(
+      <div className="col-lg-4">
+        <div className="countryCard">
+          <h3 className="countryCard__country">{this.props.api[i].country}</h3>
+          <div>
+            <img src={"https://www.countryflags.io/"+ countryCode +"/flat/64.png"}></img>
+          </div>
+          <span>Infected now:</span>
+          <h1 className="countryCard__title">{this.props.api[i].cases.active}</h1>
+        </div>
+      </div>
+      )
+    }
+    return (
+      <div className="row">
+        {items}
+      </div>
+    )
+  }
+}
+
+// const CountryCardContainer = (props) =>{
+
+//   return(
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      api : []
+    }
+  }
+  async componentDidMount(){
+    try{
+      const response = await fetch("https://covid-193.p.rapidapi.com/statistics", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "covid-193.p.rapidapi.com",
+          "x-rapidapi-key": "51b20adcd9msh684c652fb4ba30ap193f64jsn1b5bcc62dce8"
+        }
+      })
+      const data = await response.json();
+      this.setState({
+        api : data.response
+      })
+
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="container">
+            <CountryCard api={this.state.api} />
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
